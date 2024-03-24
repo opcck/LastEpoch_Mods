88c0d84fb2a0dffd1f5fe97eb7d59a21f2e89f89
Roland Solymosi <solymosi.roland@iongroup.com>
2024-03-24 20:22:12 +0100

    Remove remnants of old resource files handling.

--- Release_LastEpoch_Mods/Properties/AssemblyInfo.cs
+++ Release_LastEpoch_Mods/Properties/AssemblyInfo.cs
@@ -6,7 +6,6 @@ using System.Runtime.InteropServices;
 [assembly: MelonInfo(typeof(LastEpochMods.Main), "LastEpochMods", "3.1.9", "Ash")]
 [assembly: MelonGame("Eleventh Hour Games", "Last Epoch")]
 [assembly: VerifyLoaderVersion(0, 6, 1, true)]
-[assembly: MelonOptionalDependencies("System.Resources.Extensions", "System.Drawing.Common")]
 
 // L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
 // aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de

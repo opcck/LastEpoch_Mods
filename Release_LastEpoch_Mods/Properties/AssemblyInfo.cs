47e13c72b32644d146193309bdcfb028b304b92b
Roland Solymosi <solymosi.roland@iongroup.com>
2024-03-25 20:54:44 +0100

    Add missing AssemblyVersion build directive separation

--- Release_LastEpoch_Mods/Properties/AssemblyInfo.cs
+++ Release_LastEpoch_Mods/Properties/AssemblyInfo.cs
@@ -1,11 +1,14 @@
 ﻿using MelonLoader;
-using System.Reflection;
 using System.Runtime.InteropServices;
 
 
 [assembly: MelonInfo(typeof(LastEpochMods.Main), "LastEpochMods", "3.2.1", "Ash")]
 [assembly: MelonGame("Eleventh Hour Games", "Last Epoch")]
+#if NET5_0_OR_GREATER
 [assembly: VerifyLoaderVersion(0, 6, 1, true)]
+#else
+[assembly: VerifyLoaderVersion(0, 5, 7, false)]
+#endif
 
 // L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
 // aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
